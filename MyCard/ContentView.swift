6aa8fe225e9aafaed8ba709439846702af281b98
Vijay Lama <vijaylama2010@gmail.com>
2021-03-23 16:46:31 -0400

    Initial Commit

---
+++ MyCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCard
+//
+//  Created by Vijay Lama on 3/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
